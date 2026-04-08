d0678be50b8ce55b98fb3cb5fe49f06859a2fbbb
Sarthak Vyavahare <131747613+sarthhakk@users.noreply.github.com>
2026-04-08 10:41:05 +0530

    Add files via upload

---
+++ MainHillstations.java
@@ -0,0 +1,23 @@
+public class MainHillstations {
+
+    public static void main(String[] args) {
+
+        Hillstations h;
+
+        h = new Manali();
+        h.famousfor();
+        h.famousfood();
+
+        System.out.println();
+
+        h = new Ooty();
+        h.famousfor();
+        h.famousfood();
+
+        System.out.println();
+
+        h = new Munnar();
+        h.famousfor();
+        h.famousfood();
+    }
+}
